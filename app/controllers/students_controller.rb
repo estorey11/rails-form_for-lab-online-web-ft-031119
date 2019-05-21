f48177d223e69cd28cde7e51d57b3aeffdb99c84
Eric Storey <estorey@gmail.com>
2019-05-21 20:33:46 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -12,8 +12,7 @@ class StudentsController < ApplicationController
   end
 
   def create
-    @student = Student.new(student_params)
-    @student.save
+    @student = Student.create(student_params)
     redirect_to student_path(@student)
   end
 
